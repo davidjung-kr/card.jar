a482ea5ef88ebe2567e1fb4a4979094abb364153
davidjung-kr <jerryjueng@daum.net>
2020-09-27 21:19:00 +0900

    커버리지 보안

--- card/src/test/java/unit/CardObjInitialization.java
+++ card/src/test/java/unit/CardObjInitialization.java
@@ -3,13 +3,11 @@ package unit;
 import card.Card;
 import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CardObjInitialization {
 
 	@Test
-	@BeforeClass
 	public void testCardObjInitialization() {
 		Card fakeCard = new Card();
 		assertNotNull(fakeCard);
